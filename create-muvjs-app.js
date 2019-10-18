5aaa39485c4424696d7ef3c1aa8c1919657d78de
Armando Ortiz <armando@solairo.co.jp>
2019-10-18 17:40:11 +0900

    only run with git and npm

--- create-muvjs-app.js
+++ create-muvjs-app.js
@@ -1,36 +1,28 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = require('child_process').exec;
 
 const arguments = process.argv
-const app = arguments[2] || "";
+const app = arguments[2];
 
-let finished = false;
-
-async function createMuvJSApp() {
-	let stdout;
-	let stderr;
-	try{
-		({stdout, stderr } = await exec(`sh create-muvjs-app ${app}`));
-
-	}catch(e){
-		stderr = e.stderr;
-		stdout = e.stdout;
-	}finally{
-		console.log('\n');
-		if(stderr) console.error('stderr:\n', stderr);
-		if(stdout) console.log('stdout:\n', stdout);
-		finished = true;
-	}
+if(!app){
+	console.log("Usage: npm init muvjs-app my-app")
+	return
 }
 
 
-createMuvJSApp();
 
-process.stdout.write(`Creating muvjs app ${app}`);
-const loading = () => {
-	if(!finished){
-		process.stdout.write('.');
-		setTimeout(loading,1000);
-	} 
-}
-setTimeout(loading,1000);
+create = exec(`git clone https://github.com/yoyomo/muvjs-example ${app};
+	cd ${app};
+	git remote rm origin;
+	npm install;
+
+	echo "Now run:";
+	echo "  cd ${app}";
+	echo "  npm start";`);
+
+create.stdout.on('data', function (data) {
+	console.log(data.toString());
+});
+
+create.stderr.on('data', function (data) {
+	console.log(data.toString());
+});

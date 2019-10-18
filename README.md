56d7cc005c040c0e4f906ea23848bc0aa5d76ff5
Armando Ortiz <armando@solairo.co.jp>
2019-10-18 17:23:23 +0900

    as a node initializer

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # create-muvjs-app
 brew installed command to create a MuvJS app
 
-## How to install
+## How to use
+
+### Brew
 ```bash
 brew update
 brew tap yoyomo/brewery
 brew install yoyomo/brewery/create-muvjs-app
+
+create-muvjs-app my-app
+cd my-app
+npm start
 ```
 
-## How to use
+### NPM
 ```bash
-create-muvjs-app my-app
+npm init muvjs-app my-app
 cd my-app
 npm start
 ```
---
+++ create-muvjs-app.js
@@ -0,0 +1,36 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const arguments = process.argv
+const app = arguments[2] || "";
+
+let finished = false;
+
+async function createMuvJSApp() {
+	let stdout;
+	let stderr;
+	try{
+		({stdout, stderr } = await exec(`./create-muvjs-app ${app}`));
+
+	}catch(e){
+		stderr = e.stderr;
+		stdout = e.stdout;
+	}finally{
+		console.log('\n');
+		if(stderr) console.error('stderr:\n', stderr);
+		if(stdout) console.log('stdout:\n', stdout);
+		finished = true;
+	}
+}
+
+
+createMuvJSApp();
+
+process.stdout.write(`Creating muvjs app ${app}`);
+const loading = () => {
+	if(!finished){
+		process.stdout.write('.');
+		setTimeout(loading,1000);
+	} 
+}
+setTimeout(loading,1000);
